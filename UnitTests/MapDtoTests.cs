42d11b64d3d2bdff51a69effd2299adc29b54766
Sam Magura <srmagura@gmail.com>
2021-07-22 11:55:15 -0400

    fix record issue

---
+++ UnitTests/MapDtoTests.cs
@@ -0,0 +1,28 @@
+﻿using ITI.TypeScriptDtoGenerator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class MapDtoTests
+    {
+        private record Identity
+        {
+            public Guid Guid { get; }
+        }
+
+        [TestMethod]
+        public void ItIgnoresCompilerGeneratedProperties()
+        {
+            var dtoString = DtoGenerator.GenerateDtoToString(typeof(Identity), new(), null);
+
+            Assert.IsTrue(dtoString.Contains("guid: string"));
+            Assert.IsFalse(dtoString.Contains("equalityContract"));
+        }
+    }
+}
