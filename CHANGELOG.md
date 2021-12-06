c2357b9e348c49fca6c5fd229e1fb45b5d3d3f46
Sam Magura <srmagura@gmail.com>
2021-12-06 16:31:58 -0500

    Arrays are now mapped correctly

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 @@@ = braking change
 
+## 1.2.1
+
+- C# arrays are now translated correctly
+
 ## 1.2.0
 
 - @@@ Upgrade to .NET 6
--- UnitTests/MapTypeTests.cs
+++ UnitTests/MapTypeTests.cs
@@ -2,80 +2,100 @@ using ITI.TypeScriptDtoGenerator;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Namotion.Reflection;
 
-namespace UnitTests
+namespace UnitTests;
+
+[TestClass]
+public class MapTypeTests
 {
-    [TestClass]
-    public class MapTypeTests
+    [TestMethod]
+    public void ItMapsPrimitiveTypes()
     {
-        private class TestDto
-        {
-            public string? NullableString { get; set; }
-            public List<string?>? NullableStringList { get; set; }
-        }
+        var unknownTypes = new List<Type>();
 
-        [TestMethod]
-        public void ItMapsPrimitiveTypes()
-        {
-            var unknownTypes = new List<Type>();
+        Assert.AreEqual("string", DtoGenerator.MapType(typeof(string).ToContextualType(), unknownTypes));
+        Assert.AreEqual("number", DtoGenerator.MapType(typeof(int).ToContextualType(), unknownTypes));
+        Assert.AreEqual("number", DtoGenerator.MapType(typeof(double).ToContextualType(), unknownTypes));
+        Assert.AreEqual("boolean", DtoGenerator.MapType(typeof(bool).ToContextualType(), unknownTypes));
+        Assert.AreEqual("string", DtoGenerator.MapType(typeof(DateTime).ToContextualType(), unknownTypes));
+        Assert.AreEqual("string", DtoGenerator.MapType(typeof(DateTimeOffset).ToContextualType(), unknownTypes));
+        Assert.AreEqual("string", DtoGenerator.MapType(typeof(TimeSpan).ToContextualType(), unknownTypes));
 
-            Assert.AreEqual("string", DtoGenerator.MapType(typeof(string).ToContextualType(), unknownTypes));
-            Assert.AreEqual("number", DtoGenerator.MapType(typeof(int).ToContextualType(), unknownTypes));
-            Assert.AreEqual("number", DtoGenerator.MapType(typeof(double).ToContextualType(), unknownTypes));
-            Assert.AreEqual("boolean", DtoGenerator.MapType(typeof(bool).ToContextualType(), unknownTypes));
-            Assert.AreEqual("string", DtoGenerator.MapType(typeof(DateTime).ToContextualType(), unknownTypes));
-            Assert.AreEqual("string", DtoGenerator.MapType(typeof(DateTimeOffset).ToContextualType(), unknownTypes));
-            Assert.AreEqual("string", DtoGenerator.MapType(typeof(TimeSpan).ToContextualType(), unknownTypes));
+        Assert.AreEqual(0, unknownTypes.Count);
+    }
 
-            Assert.AreEqual(0, unknownTypes.Count);
-        }
+    [TestMethod]
+    public void ItMapsNullableValueTypes()
+    {
+        var unknownTypes = new List<Type>();
 
-        [TestMethod]
-        public void ItMapsNullableValueTypes()
-        {
-            var unknownTypes = new List<Type>();
+        Assert.AreEqual("number | null | undefined", DtoGenerator.MapType(typeof(int?).ToContextualType(), unknownTypes));
+        Assert.AreEqual("number | null | undefined", DtoGenerator.MapType(typeof(double?).ToContextualType(), unknownTypes));
 
-            Assert.AreEqual("number | null | undefined", DtoGenerator.MapType(typeof(int?).ToContextualType(), unknownTypes));
-            Assert.AreEqual("number | null | undefined", DtoGenerator.MapType(typeof(double?).ToContextualType(), unknownTypes));
+        Assert.AreEqual(0, unknownTypes.Count);
+    }
 
-            Assert.AreEqual(0, unknownTypes.Count);
-        }
+    [TestMethod]
+    public void ItMapsLists()
+    {
+        var unknownTypes = new List<Type>();
 
-        [TestMethod]
-        public void ItMapsLists()
-        {
-            var unknownTypes = new List<Type>();
+        Assert.AreEqual(
+            "number[]",
+            DtoGenerator.MapType(typeof(List<int>).ToContextualType(), unknownTypes)
+        );
+        Assert.AreEqual(
+            "(number | null | undefined)[]",
+            DtoGenerator.MapType(typeof(List<int?>).ToContextualType(), unknownTypes)
+        );
 
-            Assert.AreEqual(
-                "number[]", 
-                DtoGenerator.MapType(typeof(List<int>).ToContextualType(), unknownTypes)
-            );
-            Assert.AreEqual(
-                "(number | null | undefined)[]", 
-                DtoGenerator.MapType(typeof(List<int?>).ToContextualType(), unknownTypes)
-            );
+        Assert.AreEqual(0, unknownTypes.Count);
+    }
 
-            Assert.AreEqual(0, unknownTypes.Count);
-        }
+    [TestMethod]
+    public void ItMapsArrays()
+    {
+        var unknownTypes = new List<Type>();
 
-        [TestMethod]
-        public void ItMapsNullableReferenceTypes()
-        {
-            var unknownTypes = new List<Type>();
+        Assert.AreEqual(
+            "number[]",
+            DtoGenerator.MapType(typeof(int[]).ToContextualType(), unknownTypes)
+        );
+        Assert.AreEqual(
+            "(number | null | undefined)[]",
+            DtoGenerator.MapType(typeof(int?[]).ToContextualType(), unknownTypes)
+        );
+        Assert.AreEqual(
+            "number[]",
+            DtoGenerator.MapType(typeof(double[]).ToContextualType(), unknownTypes)
+        );
 
-            var properties = typeof(TestDto).GetContextualProperties();
-            var nullableString = properties.Single(p => p.Name == "NullableString");
-            var nullableStringList = properties.Single(p => p.Name == "NullableStringList");
+        Assert.AreEqual(0, unknownTypes.Count);
+    }
 
-            Assert.AreEqual(
-                "string | null | undefined",
-                DtoGenerator.MapType(nullableString.PropertyType, unknownTypes)
-            );
-            Assert.AreEqual(
-                "(string | null | undefined)[] | null | undefined",
-                DtoGenerator.MapType(nullableStringList.PropertyType, unknownTypes)
-            );
+    private class TestDto
+    {
+        public string? NullableString { get; set; }
+        public List<string?>? NullableStringList { get; set; }
+    }
 
-            Assert.AreEqual(0, unknownTypes.Count);
-        }
+    [TestMethod]
+    public void ItMapsNullableReferenceTypes()
+    {
+        var unknownTypes = new List<Type>();
+
+        var properties = typeof(TestDto).GetContextualProperties();
+        var nullableString = properties.Single(p => p.Name == "NullableString");
+        var nullableStringList = properties.Single(p => p.Name == "NullableStringList");
+
+        Assert.AreEqual(
+            "string | null | undefined",
+            DtoGenerator.MapType(nullableString.PropertyType, unknownTypes)
+        );
+        Assert.AreEqual(
+            "(string | null | undefined)[] | null | undefined",
+            DtoGenerator.MapType(nullableStringList.PropertyType, unknownTypes)
+        );
+
+        Assert.AreEqual(0, unknownTypes.Count);
     }
 }
